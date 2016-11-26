b4bf12b51c5928e3c75c21c901f84c5679ef24a3
IgorLapochkin <xzanich@gmail.com>
2016-11-27 02:46:46 +0300

    Style enhancements and logic.

--- project1/js/custom.js
+++ project1/js/custom.js
@@ -11,13 +11,23 @@ $(function () {
   //Fix nav on scroll
   var headerHeight = $('#js-banner').height() + $('#js-header-wrapper').height();
   $(document).on('scroll', function () {
-    var documentScroll = $(this).scrollTop();
+    var documentScroll = $(this).scrollTop() + 40;
     if (documentScroll > headerHeight) {
       $('#js-nav').addClass('fixed');
     } else {
       $('#js-nav').removeClass('fixed');
     }
+  });
+
+  //Smooth scroll to links
+  $('.header_nav a').on('click', function (e) {
+    e.preventDefault();
+
+    var currentBlock = $(this).attr('href');
+    var currentBlockOffset = $(currentBlock).offset().top;
+
+    $('html, body').animate({
+      scrollTop: currentBlockOffset - 50
+    }, 1000);
   })
-  
-  
 });
